19110cf640a4e8387b050ea4f90bd6e59ab0205e
wang <www>
2019-09-25 21:21:59 +0800

    提交

--- swagger/src/main/java/com/qf/Apstatic.java
+++ swagger/src/main/java/com/qf/Apstatic.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Apstatic {
     public static void main(String[] args) {
         SpringApplication.run(Apstatic.class,args);
+        System.out.println("123");
     }
 }
